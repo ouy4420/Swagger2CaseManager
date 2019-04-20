05ccc26c27e389857507ba9a8da2f46c71e2a155
TigerJlx <2274701670@qq.com>
2019-04-20 15:42:21 +0800

    swagger.json的导入方式设计成load_by_file和load_by_url两种形式

--- SwaggerToCase/utils.py
+++ SwaggerToCase/utils.py
@@ -1,4 +1,7 @@
 import requests
+import logging
+import sys
+import json
 
 try:
     from json.decoder import JSONDecodeError
@@ -6,15 +9,25 @@ except ImportError:
     JSONDecodeError = ValueError
 
 
-def load_item(file_path=None):
-    # with open(file_path, "r", encoding="utf-8-sig") as f:
-    #     try:
-    #         content_json = json.loads(f.read())
-    #         return content_json
-    #     except (KeyError, TypeError):
-    #         logging.error("swagger file content error: {}".format(file_path))
-    #         sys.exit(1)
-    url_json = 'http://192.168.1.107:5000/swagger.json'  # json swagger url地址
-    content_json = requests.get(url_json).json()
-    return content_json
+def load_by_url(swagger_josn_url=None):
+    '''
+    :param swagger_josn_url: json swagger url地址
+    :return: swagger dict 对象
+    '''
+    try:
+        swagger_josn_url = swagger_josn_url or 'http://192.168.1.107:5000/swagger.json'
+        content_json = requests.get(swagger_josn_url).json()
+        return content_json
+    except Exception:
+        logging.error("swagger file content error: {}".format(swagger_josn_url))
+        sys.exit(1)
 
+
+def load_by_file(file_path):
+    with open(file_path, "r", encoding="utf-8-sig") as f:
+        try:
+            content = json.loads(f.read())
+            return content
+        except (KeyError, TypeError):
+            logging.error("swagger file content error: {}".format(file_path))
+            sys.exit(1)
