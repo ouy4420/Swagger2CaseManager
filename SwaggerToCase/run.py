62c00f9e179835813e536d8d1811e621a18f95f3
TigerJlx <2274701670@qq.com>
2019-04-22 17:38:40 +0800

    重新定义SwaggerParser的输入配置参数

--- SwaggerToCase/run.py
+++ SwaggerToCase/run.py
@@ -4,21 +4,29 @@ import os
 import logging
 
 
-def start(url_or_file, swagger_name):
+def start(file_or_url, swagger_name):
     cwd = os.getcwd()
     test_pro_path = os.path.join(cwd, 'TestProject')
     log_level = getattr(logging, 'debug'.upper())
     logging.basicConfig(level=log_level)
-    s2case = SwaggerParser(url_or_file)
+
     apifilepath = os.path.join(test_pro_path, 'api')
     testcase_dir = os.path.join(test_pro_path, 'testcases\\{}'.format(swagger_name))
-    api_file = r"{}\{}.yml".format(apifilepath, swagger_name)
-    s2case.execute(testcase_dir, api_file, 'yml')
+    api_file = r"{}\{}".format(apifilepath, swagger_name)
+    config = {
+        "file_or_url": file_or_url,
+        "testcase_dir": testcase_dir,
+        "api_file": api_file,
+        "file_type": "YAML"
+    }
+    s2case = SwaggerParser(config)
+    s2case.execute()
+
     run(test_pro_path, [swagger_name])
 
 
 if __name__ == '__main__':
-    url_or_file = r'C:\Users\Administrator\PycharmProjects\Swagger2Case\json_files\aa.json'
+    url_or_file = r'C:\Users\Administrator\PycharmProjects\Swagger2Case\json_files\haha.json'
     # url_or_file = 'http://192.168.1.107:5000/swagger.json'
     swagger_name = 'MyTest'
     start(url_or_file, swagger_name)
