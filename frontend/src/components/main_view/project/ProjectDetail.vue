a9c00699d55cc14aef2737c2d209943f709c59b7
TigerJlx <2274701670@qq.com>
2019-05-05 10:03:02 +0800

    增加项目详情组件 parts：接口数、测试用例数、环境配置数和历史报告数

---
+++ frontend/src/components/main_view/project/ProjectDetail.vue
@@ -0,0 +1,137 @@
+<template>
+  <div>
+    <ul class="title-project">
+      <li class="title-li" title="Test Project">
+        <b>{{projectInfo.name}}</b>
+        <b class="desc-li">{{projectInfo.desc}}</b>
+      </li>
+    </ul>
+
+    <ul class="project_detail">
+      <li class="pull-left" @click="trans_router('APIView')">
+        <p class="title-p"><i class="iconfont">&#xe74a;</i> &nbsp;{{projectInfo.len_apis}} 个接口</p>
+        <p class="desc-p">接口总数</p>
+      </li>
+
+      <li class="pull-left"  @click="trans_router('AutoTest')">
+        <p class="title-p"><i class="iconfont">&#xe6da;</i> &nbsp;{{projectInfo.len_cases}} 个用例</p>
+        <p class="desc-p">用例集总数</p>
+      </li>
+
+      <li class="pull-left"  @click="trans_router('GlobalEnv')">
+        <p class="title-p"><i class="iconfont">&#xe609;</i> &nbsp;{{projectInfo.len_envir}} 套环境</p>
+        <p class="desc-p">环境总数</p>
+      </li>
+
+      <li class="pull-left"  @click="trans_router('Reports')">
+        <p class="title-p"><i class="iconfont">&#xe66e;</i> &nbsp;{{projectInfo.len_report}} 个报告</p>
+        <p class="desc-p">测试报告总数</p>
+      </li>
+    </ul>
+
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ProjectDetail",
+    data() {
+      return {
+        projectInfo: {}
+      }
+    },
+    methods: {
+      trans_router(routerName){
+        this.$store.commit('setRouterName', routerName);
+        this.setLocalValue("routerName", routerName);
+        this.$router.push({name: routerName});
+      },
+      success(resp) {
+        this.$notify({
+          message: resp["msg"],
+          type: 'success',
+          duration: 1000
+        });
+      },
+      failure(resp) {
+        this.$notify.error({
+          message: resp.msg,
+          duration: 1000
+        });
+      },
+
+      getProjectDetail() {
+        const project_id = this.$route.params.id;
+        this.$api.getProjectDetail(project_id).then(res => {
+          this.projectInfo = res
+        })
+      }
+    },
+    mounted() {
+      this.getProjectDetail();
+    }
+  }
+</script>
+
+<style scoped>
+
+
+  .desc-p {
+    padding-top: 10px;
+    font-size: 12px;
+    color: #b6b6b6;
+  }
+
+  .title-p {
+    font-size: 18px;
+    margin-top: 10px;
+  }
+
+  .title-project li a {
+    font-size: 12px;
+    text-decoration: none;
+    color: #a3a3a3;
+    margin-left: 20px;
+
+  }
+
+  .pull-left {
+    float: left;
+    margin-left: 10px;
+  }
+
+  .project_detail li {
+    margin-top: 10px;
+    text-indent: 20px;
+    display: inline-block;
+    height: 90px;
+    width: calc(20% - 1.5px);
+    border: 1px solid #ddd;
+  }
+
+  .project_detail {
+    height: 100px;
+    margin-top: 20px;
+  }
+
+  .title-project {
+    margin-top: 40px;
+    margin-left: 10px;
+  }
+
+  ul li {
+    list-style: none;
+  }
+
+  .title-li {
+    font-size: 24px;
+    color: #607d8b;
+  }
+
+  .desc-li {
+    margin-top: 10px;
+    color: #b6b6b6;
+    font-size: 14px;
+  }
+</style>
