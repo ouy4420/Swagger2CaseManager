feadf6087c0461cdcefd2ef5ec36068f198ae836
TigerJlx <2274701670@qq.com>
2019-05-05 10:03:37 +0800

    调整侧边导航条的parts

--- frontend/src/components/home/components/Side.vue
+++ frontend/src/components/home/components/Side.vue
@@ -9,26 +9,14 @@
         @select="select"
     >
         <el-menu-item index="ProjectList">
-            <i class="iconfont" >&#xe631;</i>&nbsp;&nbsp;首  页
+            <!--<i class="iconfont" >&#xe631;</i>-->
+          项目列表
         </el-menu-item>
 
-       <!-- <el-submenu index="ApiTest">
-            <template slot="title">
-                <i class="el-icon-view"></i>
-                <span slot="title">接口自动化</span>
-            </template>-->
-
-            <!--<el-menu-item-group>-->
-                <el-menu-item v-for="item of side_menu" :index="item.url" :key="item.url" :disabled="$store.state.routerName === 'ProjectList'">
-                    <span class="iconfont" v-html="item.code"></span>&nbsp;&nbsp;{{item.name}}
-                </el-menu-item>
-           <!-- </el-menu-item-group>
-        </el-submenu>-->
-      <!--  <el-menu-item index="Pressure" disabled>
-            &nbsp;<span class="iconfont">&#xe61f;</span>&nbsp;&nbsp;压力测试
-        </el-menu-item>-->
-
-
+        <el-menu-item v-for="item of side_menu" :index="item.url" :key="item.url" :disabled="$store.state.routerName === 'ProjectList'">
+            <!--<span class="iconfont" v-html="item.code"></span>  -->
+          {{item.name}}
+        </el-menu-item>
     </el-menu>
 </template>
 
@@ -39,13 +27,10 @@
             return {
                 side_menu: [
                     {name: "项目概况", url: "ProjectDetail", code: "&#xe64a;"},
-                    {name: "API 模板", url: "RecordApi", code: "&#xe74a;"},
+                    {name: "API 接口", url: "APIView", code: "&#xe74a;"},
                     {name: "测试用例", url: "AutoTest", code: "&#xe6da;"},
-                    {name: "配置管理", url: "RecordConfig", code: "&#xee32;"},
-                    {name: "全局变量", url: "GlobalEnv", code: "&#xe692;"},
-                    {name: "域名管理", url: "HostIP", code: "&#xe609;"},
+                    {name: "环境变量", url: "GlobalEnv", code: "&#xe692;"},
                     {name: "驱动代码", url: "DebugTalk", code: "&#xe7ca;"},
-                    {name: "定时任务", url: "Task", code: "&#xe61e;"},
                     {name: "历史报告", url: "Reports", code: "&#xe66e;"}
                 ],
             }
