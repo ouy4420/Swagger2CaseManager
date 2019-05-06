1f67a700cb8ba03fe8716ef2631bed2cdf31944e
TigerJlx <2274701670@qq.com>
2019-05-06 11:36:22 +0800

    config.vue: 解决几个bug 1、新增数据受编辑数据影响，导致新增变成编辑 解决办法：使用dialog的close回调函数 2、没有case_id 解决办法：后来在提供currentCase时，加上case_id

--- frontend/src/components/main_view/test_case/case_content/config/config.vue
+++ frontend/src/components/main_view/test_case/case_content/config/config.vue
@@ -1,12 +1,12 @@
 <template>
 
-<div>
+  <div>
     <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
-    <el-tab-pane label="Name" name="name">
-      {{this.$store.state.currentCase['config'].config.name}}
-    </el-tab-pane>
-    <el-tab-pane label="Parameters" name="parameters">
-      <el-table
+      <el-tab-pane label="Name" name="name">
+        {{this.$store.state.currentCase['config'].config.name}}
+      </el-tab-pane>
+      <el-tab-pane label="Parameters" name="parameters">
+        <el-table
           highlight-current-row
           :data="this.$store.state.currentCase['config'].config.parameters"
           border
@@ -38,47 +38,46 @@
             </template>
           </el-table-column>
         </el-table>
-    </el-tab-pane>
-    <el-tab-pane label="Request" name="request">
-      角色管理
-    </el-tab-pane>
-    <el-tab-pane label="Variables" name="variables">
-       <div>
-        <div style="">
-          <el-button type="primary"
-                     size="small"
-                     @click="dialogVisible = true"
-                      icon="el-icon-circle-plus">
-            添加Variable
-          </el-button>
-
-
+      </el-tab-pane>
+      <el-tab-pane label="Request" name="request">
+        角色管理
+      </el-tab-pane>
+      <el-tab-pane label="Variables" name="variables">
+        <div>
           <el-dialog
-            title="添加Variable"
-            :visible.sync="dialogVisible"
-            width="30%"
-            align="center"
-          >
-            <el-form :model="variableForm"
-                     :rules="rules"
-                     ref="variableForm"
-                     label-width="110px"
-                     class="project">
-              <el-form-item label="变量名称" prop="name">
-                <el-input v-model="variableForm.key" clearable></el-input>
-              </el-form-item>
-              <el-form-item label="变量值" prop="url">
-                <el-input v-model="variableForm.value" clearable></el-input>
-              </el-form-item>
-            </el-form>
-            <span slot="footer" class="dialog-footer">
-                        <el-button @click="dialogVisible = false">取消</el-button>
-                        <el-button type="primary" @click="handleConfirm('variableForm')">确 定</el-button>
+              :title="DialogTitle"
+              :visible.sync="DialogVisible"
+              width="30%"
+              align="center"
+              @close="clear_variable_form"
+            >
+              <el-form :model="variableForm"
+                       :rules="rules"
+                       ref="variableForm"
+                       label-width="110px"
+                       class="project">
+                <el-form-item label="变量名称" prop="key">
+                  <el-input v-model="variableForm.key" clearable></el-input>
+                </el-form-item>
+                <el-form-item label="变量值" prop="value">
+                  <el-input v-model="variableForm.value" clearable></el-input>
+                </el-form-item>
+              </el-form>
+              <span slot="footer" class="dialog-footer">
+                        <el-button @click="DialogVisible = false">取消</el-button>
+                        <el-button type="primary" @click="handleConfirm()">确 定</el-button>
                       </span>
-          </el-dialog>
+            </el-dialog>
+          <div style="">
+            <el-button type="primary"
+                       size="small"
+                       @click="DialogVisible = true; DialogTitle='添加Variable'"
+                       icon="el-icon-circle-plus">
+              添加Variable
+            </el-button>
+          </div>
         </div>
-      </div>
-       <el-table
+        <el-table
           highlight-current-row
           :data="this.$store.state.currentCase['config'].config.variables"
           border
@@ -109,7 +108,7 @@
               <span>{{ scope.row.value }}</span>
             </template>
           </el-table-column>
-                   <el-table-column
+          <el-table-column
             label="操作"
             align="center"
           >
@@ -117,32 +116,8 @@
               <el-button
                 size="medium"
                 type="primary"
-                @click="handleEdit(scope.$index, scope.row)">编辑
+                @click="DialogVisible = true; DialogTitle='编辑Variable'; handleEdit(scope.$index, scope.row)">编辑
               </el-button>
-
-              <el-dialog
-                title="编辑Variable"
-                :visible.sync="editVisible"
-                width="30%"
-              >
-                <el-form :model="variableForm"
-                         :rules="rules"
-                         ref="variableForm"
-                         label-width="100px"
-                         class="project">
-                  <el-form-item label="变量名称" prop="name">
-                    <el-input v-model="variableForm.key" clearable></el-input>
-                  </el-form-item>
-                  <el-form-item label="变量值" prop="desc">
-                    <el-input v-model="variableForm.value" clearable></el-input>
-                  </el-form-item>
-                </el-form>
-                <span slot="footer" class="dialog-footer">
-                        <el-button @click="editVisible = false">取 消</el-button>
-                        <el-button type="primary" @click="handleConfirm('variableForm')">确 定</el-button>
-                      </span>
-              </el-dialog>
-
 
               <el-button
                 size="medium"
@@ -152,9 +127,9 @@
             </template>
           </el-table-column>
         </el-table>
-    </el-tab-pane>
-  </el-tabs>
-</div>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 
 </template>
 
@@ -165,15 +140,14 @@
     data() {
       return {
         activeName: 'name',
-        editVisible: false,
-        dialogVisible: false,
+        DialogTitle: "",
+        DialogVisible: false,
         current_case_id: null,
         variableForm: {
           key: '',
           value: '',
           id: '',
-          config_id: '',
-          case_id: ''
+          config_id: ''
         },
         rules: {
           key: [
@@ -189,24 +163,30 @@
     },
     methods: {
       handleClick(tab, event) {
-          console.log(1,tab);
-          console.log(2, event);
-        },
+        console.log(1, tab);
+        console.log(2, event);
+      },
       handleEdit(index, row) {
-        console.log("row data: ",  row);
+        console.log("row data: ", row);
         this.editVisible = true;  // 弹出编辑框
         this.variableForm.key = row['key'];
-        this.variableForm.value = JSON.stringify(row['value']);
+         if (typeof row['value'] == "object"){
+          this.variableForm.value = JSON.stringify(row['value']);
+        }else{
+          this.variableForm.value = row['value'];
+        }
         this.variableForm.id = row['id'];
         this.variableForm.config_id = row['config_id'];
-        this.variableForm.case_id = row['case_id'];
       },
       handleDelete(index, row) {
         this.variableForm.key = row['key'];
-        this.variableForm.value = JSON.stringify(row['value']);
+        if (typeof row['value'] == "object"){
+          this.variableForm.value = JSON.stringify(row['value']);
+        }else{
+          this.variableForm.value = row['value'];
+        }
         this.variableForm.id = row['id'];
         this.variableForm.config_id = row['config_id'];
-        this.variableForm.case_id = row['case_id'];
         // 弹出确认警告提示框
         this.$confirm('此操作将永久删除该项目, 是否继续?', '提示', {
           confirmButtonText: '确定',
@@ -221,45 +201,53 @@
             } else {
               this.failure(resp);
             }
+            this.variableForm = {
+                key: '',
+                value: '',
+                id: '',
+                config_id: ''
+              };
           })
         })
       },
-      handleConfirm(formName) {
-        this.$refs[formName].validate((valid) => {
+      clear_variable_form(){
+        this.variableForm = {
+                key: '',
+                value: '',
+                id: '',
+                config_id: ''
+              };
+      },
+      handleConfirm() {
+        console.log(1111, this.$refs["variableForm"])
+        this.$refs["variableForm"].validate((valid) => {
           if (valid) {
             // 新建或编辑框中的数据校验通过后，将弹框隐藏掉
-            this.dialogVisible = false;
-            this.editVisible = false;
+            this.DialogVisible = false;
             let obj;
-            console.log("1", this.variableForm);
             if (this.variableForm.id === '') {
               this.variableForm.config_id = this.$store.state.currentCase['config'].config_id;
-              console.log("2", this.variableForm);
               obj = this.$api.addVariable(this.variableForm);     // 没有就新建
             } else {
               obj = this.$api.updateVariable(this.variableForm);  // 有就更新
             }
             // 给http response挂载一个处理的钩子
-            console.log("1-------------------------------")
             obj.then(resp => {
-              console.log("1-------------------------------")
               if (resp.success) {
                 this.success(resp);
                 this.get_case();
               } else {
                 this.failure(resp);
               }
-              console.log("2-------------------------------")
-              this.variableForm.key = '';
-              this.variableForm.value = '';
-              this.variableForm.id = '';
-              this.variableForm.config_id = '';
+              this.clear_variable_form()
             })
-          } else {
+          }
+          else {
+            this.DialogVisible = true;
             if (this.variableForm.id !== '') {
-              this.editVisible = true;  // 已经存在显示编辑框
+              this.DialogTitle = "编辑Variable";  // 已经存在显示编辑框
             } else {
-              this.dialogVisible = true;  // 不存在显示新建框
+              this.dialogVisible = "新增Variable";  // 不存在显示新建框
             }
             return false;
           }
@@ -280,25 +268,29 @@
         });
       },
       get_case() {
-        console.log("in get_case2", this.variableForm)
-        this.$api.getCaseDetail(this.variableForm.case_id).then(resp => {
-          console.log("in getCaseDetail", resp)
+        console.log("in get_case2", this.variableForm);
+        console.log("in get_case is.$store.state.currentCase", this.$store.state.currentCase);
+        var currentCaseID = this.$store.state.currentCase['config'].case_id;
+        this.$api.getCaseDetail(currentCaseID).then(resp => {
+          console.log("in getCaseDetail", resp);
           this.$store.commit('setCurrentCase', resp);
         });
       }
     },
     mounted() {
       var obj = this;
-      function temp_func(){
-          var state = obj.$store.state;
-      var user = state.user;
-      var token = state.token;
-      var currentCase = state.currentCase;
-      obj.config = currentCase["config"];
-      console.log(666, "user", user);
-      console.log(666, "token", token);
-      console.log(666, "currentCase", currentCase)
+
+      function temp_func() {
+        var state = obj.$store.state;
+        var user = state.user;
+        var token = state.token;
+        var currentCase = state.currentCase;
+        obj.config = currentCase["config"];
+        console.log(666, "user", user);
+        console.log(666, "token", token);
+        console.log(666, "currentCase", currentCase)
       }
+
       // setTimeout(temp_func,1000);
       // // this.config = this.$store.state.currentCase;
 
