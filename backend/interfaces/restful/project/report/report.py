ff2bcede1e982c2aedde5e970f1e6590dd4b5172
TigerJlx <2274701670@qq.com>
2019-05-08 11:48:26 +0800

    后台实现report restful接口

---
+++ backend/interfaces/restful/project/report/report.py
@@ -0,0 +1,95 @@
+from flask import make_response, jsonify
+from flask_restful import Resource, reqparse
+from backend.models.models import Report, Project
+from backend.models.curd import CURD, session
+
+curd = CURD()
+parser = reqparse.RequestParser()
+parser.add_argument('id', type=int)
+parser.add_argument('page', type=int)
+parser.add_argument('current_time', type=str)
+parser.add_argument('name', type=str)
+parser.add_argument('tester', type=str)
+parser.add_argument('description', type=str)
+
+
+class ReportItem(Resource):
+    def get(self, report_id):
+        try:
+            report = session.query(Report).filter_by(id=report_id).first()
+            rst = make_response(jsonify({"success": True,
+                                         "msg": "",
+                                         "render_content": report.render_content
+                                         }))
+            return rst
+        except Exception as e:
+            print('InternalError:', e)
+            session.rollback()
+            return make_response(jsonify({"success": False, "msg": "sql error ==> rollback!"}))
+
+    def delete(self, report_id):
+        status, msg = curd.delete_report(report_id)
+        rst = make_response(jsonify({"success": status, "msg": msg}))
+        return rst
+
+    def patch(self, report_id):
+        try:
+            args = parser.parse_args()
+            print(args)
+            status, msg = curd.update_report(args)
+            rst = make_response(jsonify({"success": status, "msg": msg}))
+            return rst
+        except Exception as e:
+            rst = make_response(jsonify({"success": False, "msg": e}))
+            return rst
+
+    def post(self, report_id):
+        pass
+
+
+def get_page(page, project_id):
+    all_rets = session.query(Report).filter_by(project_id=project_id).all()
+    length = len(all_rets)
+    per_page = 5
+    pages = length // per_page
+    if length % per_page > 0:
+        pages += 1
+    offset = per_page * (page - 1)
+    page_rets = session.query(Report).filter_by(project_id=project_id).limit(per_page).offset(offset).all()
+    return all_rets, page_rets, pages
+
+
+class ReportList(Resource):
+    def get(self):
+        try:
+            args = parser.parse_args()
+            print("args: ", args)
+            id, page = args["id"], args["page"]
+            report_list = []
+            all_rets, page_rets, pages = get_page(page, id)
+            for report in page_rets:
+                index = all_rets.index(report) + 1
+                report_list.append(
+                    {"index": index,
+                     "id": report.id,
+                     "name": report.name,
+                     "current_time": report.current_time,
+                     "tester": report.tester,
+                     "description": report.description})
+            page_previous, page_next = None, None
+            if page > 1:
+                page_previous = page - 1
+            if page + 1 <= pages:
+                page_next = page + 1
+            project = session.query(Project).filter_by(id=id).first()
+            rst = make_response(jsonify({
+                "success": True,
+                "reportList": report_list,
+                "projectInfo": {"name": project.name, "desc": project.desc},
+                "page": {"page_now": page,
+                         "page_previous": page_previous,
+                         "page_next": page_next}}))
+            return rst
+        except Exception as e:
+            rst = make_response(jsonify({"success": False, "msg": e}))
+            return rst
