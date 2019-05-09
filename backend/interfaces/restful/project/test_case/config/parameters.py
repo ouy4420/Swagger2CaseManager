63e10d1b83bbbc949fa4b20614a597d2cec3cd1c
TigerJlx <2274701670@qq.com>
2019-05-09 21:54:59 +0800

    根据“API数据库表value_type字段”对应修改patch 和 post部分

--- backend/interfaces/restful/project/test_case/config/parameters.py
+++ backend/interfaces/restful/project/test_case/config/parameters.py
@@ -1,9 +1,10 @@
 from flask import make_response, jsonify
 from flask_restful import Resource, reqparse
-from sqlalchemy.exc import InternalError, InterfaceError
+
 from backend.models.models import Parameters
 from backend.models.curd import ParametersCURD, session
 
+import json
 
 curd = ParametersCURD()
 parser = reqparse.RequestParser()
@@ -11,6 +12,7 @@ parser.add_argument('id', type=str)
 parser.add_argument('config_id', type=str)
 parser.add_argument('key', type=str)
 parser.add_argument('value', type=str)
+parser.add_argument('value_type', type=str)
 
 
 class ParameterItem(Resource):
@@ -23,16 +25,13 @@ class ParameterItem(Resource):
                                          "id": parameter.id,
                                          "config_id": parameter.config_id,
                                          "key": parameter.key,
-                                         "value": parameter.value
+                                         "value": parameter.value,
+                                         "value_type": parameter.value_type
                                          }))
             return rst
-        except InternalError as e:
-            print('InternalError:', e)
+        except Exception as e:
             session.rollback()
-        except InterfaceError as e:
-            print('InterfaceError:', e)
-            session.rollback()
-            return make_response(jsonify({"success": False, "msg": "sql error ==> rollback!"}))
+            return make_response(jsonify({"success": False, "msg": "sql error ==> rollback!" + str(e)}))
 
     def delete(self):
         args = parser.parse_args()
@@ -43,6 +42,14 @@ class ParameterItem(Resource):
 
     def patch(self):
         args = parser.parse_args()
+        value, value_type = args["value"], args["value_type"]
+        if value_type == "json_list":
+            try:
+                json.loads(value)
+            except Exception as e:
+                status, msg = False, "请选择正确的parameter类型" + str(e)
+                rst = make_response(jsonify({"success": status, "msg": msg}))
+                return rst
         status, msg = curd.update_parameter(args)
         rst = make_response(jsonify({"success": status, "msg": msg}))
         return rst
@@ -50,7 +57,14 @@ class ParameterItem(Resource):
     def post(self):
         args = parser.parse_args()
         config_id = int(args["config_id"])
-        variable = {"key": args["key"], "value": args["value"]}
-        status, msg = curd.add_parameter(config_id, variable)
+        value, value_type = args["value"], args["value_type"]
+        if value_type == "json_list":
+            try:
+                json.loads(value)
+            except Exception as e:
+                status, msg = False, "请选择正确的parameter类型" + str(e)
+                rst = make_response(jsonify({"success": status, "msg": msg}))
+                return rst
+        status, msg = curd.add_parameter(config_id, args)
         rst = make_response(jsonify({"success": status, "msg": msg}))
         return rst
