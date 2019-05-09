914ef35b73e187c37cca2705d10d8bb9e0cd2156
TigerJlx <2274701670@qq.com>
2019-05-09 19:54:50 +0800

    CURD -> ExtractCURD

--- backend/interfaces/restful/project/test_case/test_step/extract.py
+++ backend/interfaces/restful/project/test_case/test_step/extract.py
@@ -2,10 +2,10 @@ from flask import make_response, jsonify
 from flask_restful import Resource, reqparse
 
 from backend.models.models import Extract
-from backend.models.curd import CURD, session
+from backend.models.curd import ExtractCURD, session
 
 
-curd = CURD()
+curd = ExtractCURD()
 parser = reqparse.RequestParser()
 parser.add_argument('id', type=str)
 parser.add_argument('step_id', type=str)
