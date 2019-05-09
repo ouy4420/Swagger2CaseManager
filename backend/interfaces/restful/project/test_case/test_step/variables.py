21e0d3ff95ff89c60ba0c0757b380128cd72829c
TigerJlx <2274701670@qq.com>
2019-05-09 19:52:17 +0800

    CURD -> VarLocalCURD

--- backend/interfaces/restful/project/test_case/test_step/variables.py
+++ backend/interfaces/restful/project/test_case/test_step/variables.py
@@ -1,9 +1,9 @@
 from flask import make_response, jsonify
 from flask_restful import Resource, reqparse
 from backend.models.models import VariablesLocal
-from backend.models.curd import CURD, session
+from backend.models.curd import VarLocalCURD, session
 
-curd = CURD()
+curd = VarLocalCURD()
 parser = reqparse.RequestParser()
 parser.add_argument('id', type=str)
 parser.add_argument('config_id', type=str)
