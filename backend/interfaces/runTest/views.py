7f34ea6d85a816e80b5f9d29dcdf6ec4d44f121c
TigerJlx <2274701670@qq.com>
2019-05-11 15:37:42 +0800

    执行测试用例时可能会因为dump_file中存在格式不正确（不符合httprunner语法标准），加入try except机制，返回错误信息给前端显示

--- backend/interfaces/runTest/views.py
+++ backend/interfaces/runTest/views.py
@@ -10,44 +10,60 @@ from SwaggerToCase.inherit import run
 import os
 import shutil
 
+
+class RunTestError(ValueError):
+    pass
+
+
 @run_test.route('/api/waykichain/run_test/', methods=['POST'])
 @login_require
 def run_test():
-    case_list = request.json.get('case_list')
-    project_id = request.json.get('project_id')
+    try:
+        # 解析参数
+        case_list = request.json.get('case_list')
+        project_id = request.json.get('project_id')
 
-    project_obj = session.query(Project).filter_by(id=project_id).first()
-    project_name = project_obj.name
-    cwd = os.getcwd()
-    config = {}
-    testcases_dir = os.path.join(cwd, r"SwaggerToCase\TestProject\testcases")
-    testapi_dir = os.path.join(cwd, r"SwaggerToCase\TestProject\api")
-    # 清空testcases_dir和testapi_dir
-    shutil.rmtree(testcases_dir)
-    shutil.rmtree(testapi_dir)
-    os.mkdir(testcases_dir)
-    os.mkdir(testapi_dir)
-    config["testcase_dir"] = os.path.join(testcases_dir, project_name)
-    config["api_file"] = os.path.join(testapi_dir, project_name)
-    config["file_type"] = "YAML"
+        cwd = os.getcwd()
 
-    case_curd = TestCaseCURD()
-    report_curd = ReportCURD()
-    case_ids, test_apis, test_cases = case_curd.retrieve_part_cases(case_list)
-    dumper = DumpFile(config, test_apis, test_cases)
-    dumper.dump_api_file()  # 写入api文件
-    dumper.dump_testcases_files()  # 写入testcase文件
-    report_list = run(os.path.join(cwd, r"SwaggerToCase\TestProject"), [project_name])
-    for report in report_list:
-        file_name, current_time, render_content = report
-        report_dict = {
-            "project_id": project_id,
-            "name": file_name,
-            "current_time": current_time,
-            "tester": project_obj.owner,
-            "description": "",
-            "render_content": render_content
-        }
-        report_curd.add_report(report_dict)
-    file_name, current_time, render_content = report_list[0]
-    return jsonify({'success': True, 'msg': '用例执行成功！', "render_content": render_content})
+        testproject_dir = os.path.join(cwd, r"SwaggerToCase\TestProject")
+        testcases_dir = os.path.join(testproject_dir, r"testcases")
+        testapi_dir = os.path.join(testproject_dir, r"api")
+        # 清空testcases_dir和testapi_dir
+        shutil.rmtree(testcases_dir)
+        shutil.rmtree(testapi_dir)
+        os.mkdir(testcases_dir)
+        os.mkdir(testapi_dir)
+
+        project_obj = session.query(Project).filter_by(id=project_id).first()
+        project_name = project_obj.name
+        config = {}
+        config["testcase_dir"] = os.path.join(testcases_dir, project_name)
+        config["api_file"] = os.path.join(testapi_dir, project_name)
+        config["file_type"] = "YAML"
+
+        case_curd = TestCaseCURD()
+        report_curd = ReportCURD()
+        case_ids, test_apis, test_cases = case_curd.retrieve_part_cases(case_list)
+        dumper = DumpFile(config, test_apis, test_cases)
+        dumper.dump_api_file()  # 写入api文件
+        dumper.dump_testcases_files()  # 写入testcase文件
+        try:
+            report_list = run(testproject_dir, [project_name])
+        except Exception as e:
+            return jsonify({'success': False, 'msg': '用例执行失败！' + str(e)})
+            # raise RunTestError('...')
+        for report in report_list:
+            file_name, current_time, render_content = report
+            report_dict = {
+                "project_id": project_id,
+                "name": file_name,
+                "current_time": current_time,
+                "tester": project_obj.owner,
+                "description": "",
+                "render_content": render_content
+            }
+            report_curd.add_report(report_dict)
+        file_name, current_time, render_content = report_list[0]
+        return jsonify({'success': True, 'msg': '用例执行成功！', "render_content": render_content})
+    except Exception as e:
+        return jsonify({'success': False, 'msg': '用例执行失败！' + str(e)})
