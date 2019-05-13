e50796394c77e5e119564fc8767e14ab060af81a
TigerJlx <2274701670@qq.com>
2019-05-13 15:44:07 +0800

    添加teststep新增接口（基于已有模板，今后也可以采取空模板的方式）

--- backend/interfaces/restful/project/test_case/test_step/teststep.py
+++ backend/interfaces/restful/project/test_case/test_step/teststep.py
@@ -5,7 +5,9 @@ from backend.models.curd import StepCURD
 curd = StepCURD()
 parser = reqparse.RequestParser()
 parser.add_argument('id', type=str)
+parser.add_argument('case_id', type=int)
 parser.add_argument('api_name', type=str)
+parser.add_argument('step_num', type=int)
 
 
 class StepItem(Resource):
@@ -22,4 +24,8 @@ class StepItem(Resource):
         return rst
 
     def post(self):
-        pass
+        args = parser.parse_args()
+        case_id, api_name, step_num = args["case_id"], args["api_name"], args["step_num"]
+        status, msg = curd.add_step(case_id, api_name, step_num)
+        rst = make_response(jsonify({"success": status, "msg": msg}))
+        return rst
