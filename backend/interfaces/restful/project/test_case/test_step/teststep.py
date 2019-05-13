5545dabbb3922a5cb9f9102dfea26d26ef6d8700
TigerJlx <2274701670@qq.com>
2019-05-13 20:59:42 +0800

    增加name参数的parse

--- backend/interfaces/restful/project/test_case/test_step/teststep.py
+++ backend/interfaces/restful/project/test_case/test_step/teststep.py
@@ -7,6 +7,7 @@ parser = reqparse.RequestParser()
 parser.add_argument('id', type=str)
 parser.add_argument('step_id', type=str)
 parser.add_argument('step_name', type=str)
+parser.add_argument('name', type=str)
 parser.add_argument('case_id', type=int)
 parser.add_argument('api_name', type=str)
 parser.add_argument('step_pos', type=int)
@@ -30,7 +31,6 @@ class StepItem(Resource):
 
     def post(self):
         args = parser.parse_args()
-        case_id, api_name, step_pos = args["case_id"], args["api_name"], args["step_pos"]
-        status, msg = curd.add_step(case_id, api_name, step_pos)
+        status, msg = curd.add_step(args)
         rst = make_response(jsonify({"success": status, "msg": msg}))
         return rst
