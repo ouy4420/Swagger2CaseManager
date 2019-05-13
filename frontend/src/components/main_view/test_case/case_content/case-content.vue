45854435ac80503b0c5e25b65894530477f2853f
TigerJlx <2274701670@qq.com>
2019-05-13 21:08:53 +0800

    增加step名称的自定义输入 对弹窗表单的输入参数进行校验（rules+messageBox）

--- frontend/src/components/main_view/test_case/case_content/case-content.vue
+++ frontend/src/components/main_view/test_case/case_content/case-content.vue
@@ -14,20 +14,30 @@
       width="30%"
       align="center"
     >
-      <!--<div v-for="api_name in apiList">{{api_name}}</div>-->
-      <el-select v-model="api_name" placeholder="API模板">
-        <el-option
-          v-for="api in apiList"
-          :key="api"
-          :label="api"
-          :value="api">
-        </el-option>
-      </el-select>
+      <el-form :model="stepForm"
+               :rules="rules"
+               label-width="110px"
+               class="project"
+               close>
+        <el-form-item label="Step名称" prop="name">
+          <el-input v-model="stepForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="API调用" prop="api_name">
+          <el-select style="width: 350px" v-model="stepForm.api_name" placeholder="选择API调用">
+            <el-option
+              v-for="api in apiList"
+              :key="api"
+              :label="api"
+              :value="api">
+            </el-option>
+          </el-select>
+        </el-form-item>
+
+      </el-form>
 
       <div slot="footer" class="dialog-footer">
         <el-button @click="DialogVisible = false">取消</el-button>
         <el-button type="primary" @click="handleConfirm()">确 定</el-button>
-        <!--<el-button type="primary" @click="innerVisible = true">打开内层 Dialog</el-button>-->
       </div>
     </el-dialog>
   </div>
@@ -49,6 +59,16 @@
         DialogVisible: false,
         api_name: null,
         apiList: [],
+        stepForm: {
+          name: "",
+          api_name: ""
+        },
+        rules: {
+          name: [
+            {required: true, message: '请输入Step名称', trigger: 'blur'},
+            {min: 1, max: 50, message: '最多不超过50个字符', trigger: 'blur'}
+          ]
+        }
       }
     },
     methods: {
@@ -61,16 +81,34 @@
         )
       },
       handleConfirm() {
+        if (this.stepForm.name === "") {
+          this.$alert('请填写Step名称', '提示', {
+            confirmButtonText: '确定',
+            callback: action => {
+
+            }
+          });
+          return
+        }
+        if (this.stepForm.api_name === "") {
+          this.$alert('请选择API调用', '提示', {
+            confirmButtonText: '确定',
+            callback: action => {
+
+            }
+          });
+          return
+        }
         var case_id = this.$store.state.currentCase.case_id;
-        var api_name = this.api_name;
-        var last_step_pos = this.$store.state.currentCase.last_step_pos + 1;
+        var step_pos = this.$store.state.currentCase.last_step_pos + 1;
         var rquest_data = {
           "case_id": case_id,
-          "api_name": api_name,
-          "step_pos": last_step_pos
+          "name": this.stepForm.name,
+          "api_name": this.stepForm.api_name,
+          "step_pos": step_pos
         };
         this.$api.addStep(rquest_data).then(resp => {
-              if (resp['success']) {
+            if (resp['success']) {
               this.success(resp);       // 弹出成功提示消息
               this.get_case();          // 重新刷新当前case数据
             } else {
@@ -78,7 +116,11 @@
             }
           }
         );
-        this. DialogVisible = false;
+        this.stepForm = {
+          name: "",
+          api_name: ""
+        };
+        this.DialogVisible = false;
 
       },
       success(resp) {
