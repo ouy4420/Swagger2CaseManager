e9c84f8b56c0eb11cc6426fa8dcf5c4c93ef863b
TigerJlx <2274701670@qq.com>
2019-05-13 16:20:46 +0800

    前端编写新增teststep部分 1、选择现有API模板-> api_name 2、获取后端传过来的last_step_pos，得到新增step的step_pos 3、获取当前case的case_id 根据前面三个参数调用后台新建test_step接口进行test_step新建

--- frontend/src/components/main_view/test_case/case_content/case-content.vue
+++ frontend/src/components/main_view/test_case/case_content/case-content.vue
@@ -4,7 +4,32 @@
     <h1 class="title">Config(全局配置):</h1>
     <config style="margin-bottom: 40px"></config>
     <h1 class="title">Teststeps(测试步骤):</h1>
+    <el-button size="medium" type="primary" @click="addStep" style="margin-bottom: 20px">
+      <i class="el-icon-circle-plus"></i>AddStep
+    </el-button>
     <teststeps></teststeps>
+    <el-dialog
+      title="添加TestStep"
+      :visible.sync="DialogVisible"
+      width="30%"
+      align="center"
+    >
+      <!--<div v-for="api_name in apiList">{{api_name}}</div>-->
+      <el-select v-model="api_name" placeholder="API模板">
+        <el-option
+          v-for="api in apiList"
+          :key="api"
+          :label="api"
+          :value="api">
+        </el-option>
+      </el-select>
+
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="DialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="handleConfirm()">确 定</el-button>
+        <!--<el-button type="primary" @click="innerVisible = true">打开内层 Dialog</el-button>-->
+      </div>
+    </el-dialog>
   </div>
 
 </template>
@@ -20,9 +45,62 @@
       "teststeps": Teststeps
     },
     data() {
-      return {}
+      return {
+        DialogVisible: false,
+        api_name: null,
+        apiList: [],
+      }
     },
-    methods: {},
+    methods: {
+      addStep() {
+        this.DialogVisible = true;
+        const project_id = this.$route.params.id;
+        this.$api.getAPIList({"id": project_id}).then(resp => {
+            this.apiList = resp["api_list"];
+          }
+        )
+      },
+      handleConfirm() {
+        var case_id = this.$store.state.currentCase.case_id;
+        var api_name = this.api_name;
+        var last_step_pos = this.$store.state.currentCase.last_step_pos + 1;
+        var rquest_data = {
+          "case_id": case_id,
+          "api_name": api_name,
+          "step_pos": last_step_pos
+        };
+        this.$api.addCase(rquest_data).then(resp => {
+              if (resp['success']) {
+              this.success(resp);       // 弹出成功提示消息
+              this.get_case();          // 重新刷新当前case数据
+            } else {
+              this.failure(resp);
+            }
+          }
+        );
+        this. DialogVisible = false;
+
+      },
+      success(resp) {
+        this.$notify({
+          message: resp["msg"],
+          type: 'success',
+          duration: 1000
+        });
+      },
+      failure(resp) {
+        this.$notify.error({
+          message: resp["msg"],
+          duration: 1000
+        });
+      },
+      get_case() {
+        var currentCaseID = this.$store.state.currentCase['config'].case_id;
+        this.$api.getCaseDetail(currentCaseID).then(resp => {
+          this.$store.commit('setCurrentCase', resp);
+        });
+      }
+    },
     mounted() {
 
     }
@@ -32,7 +110,7 @@
 <style scoped>
   .title {
     width: 100%;
-    height: 42px;
+    height: 10px;
     font-size: 30px;
     /*color: #fff;*/
     /*background: #1F5DEA;*/
