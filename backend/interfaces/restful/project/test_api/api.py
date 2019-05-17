4e61a013de3f4dc56a5ba845b28d6767fb2d42ee
TigerJlx <2274701670@qq.com>
2019-05-17 21:03:08 +0800

    1、api.py中涉及很多不在curd.py中的数据库操作，单独建立数据库的连接（与curd中的并发访问数据库） 2、添加必要的异常捕捉和日志记录

--- backend/interfaces/restful/project/test_api/api.py
+++ backend/interfaces/restful/project/test_api/api.py
@@ -1,11 +1,10 @@
+import json
 from flask import make_response, jsonify
 from flask_restful import Resource, reqparse
+
+# ----------------------------------------------------------------------------------------------------------------------
 from backend.models.models import API, Project
-from backend.models.curd import APICURD, session
-import json
-import traceback
-import logging
-mylogger = logging.getLogger("Swagger2CaseManager")
+from backend.models.curd import APICURD
 
 curd = APICURD()
 parser = reqparse.RequestParser()
@@ -13,6 +12,28 @@ parser.add_argument('id', type=int)
 parser.add_argument('page', type=int)
 parser.add_argument('api_obj', type=dict)
 parser.add_argument('api_id', type=str)
+
+# ----------------------------------------------------------------------------------------------------------------------
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("mysql+pymysql://root:ate.sqa@127.0.0.1:3306/swagger?charset=utf8",
+                       encoding='utf-8',
+                       # echo=True,
+                       isolation_level='AUTOCOMMIT',  # 加上这句解决查询数据库不更新的情况
+                       max_overflow=5,
+                       pool_size=4,
+                       pool_recycle=60 * 60 * 2,  # 设置pool_recycle参数在超时设定的时间(秒)后自动重新建立连接, 每过两小时建立一个新连接
+                       )
+
+Session = sessionmaker(bind=engine)
+session_in_api = Session()
+
+# ----------------------------------------------------------------------------------------------------------------------
+import traceback
+import logging
+
+mylogger = logging.getLogger("Swagger2CaseManager")
 
 
 def parse_api_body(api):
@@ -30,7 +51,7 @@ def parse_api_body(api):
 
 
 def get_page(page, project_id):
-    all_rets = session.query(API).filter_by(project_id=project_id).all()
+    all_rets = session_in_api.query(API).filter_by(project_id=project_id).all()
     all_rets_reverse = all_rets[::-1]
     length = len(all_rets)
     per_page = 10
@@ -38,7 +59,6 @@ def get_page(page, project_id):
     if length % per_page > 0:
         pages += 1
     offset = per_page * (page - 1)
-    # page_rets = session.query(API).filter_by(project_id=project_id).limit(per_page).offset(offset).all()
     page_rets = all_rets_reverse[offset:offset + per_page]
     return all_rets_reverse, page_rets, pages
 
@@ -53,6 +73,7 @@ class APILIst(Resource):
                 try:
                     all_rets_reverse, page_rets, pages = get_page(page, id)
                 except Exception as e:
+                    # 需要中间记录一下错误，可以按照下列做法
                     error_decription = "获取API分页数据失败！\n"
                     error_location = traceback.format_exc()
                     mylogger.error(error_decription + error_location)
@@ -84,13 +105,7 @@ class APILIst(Resource):
                     page_previous = page - 1
                 if page + 1 <= pages:
                     page_next = page + 1
-                try:
-                    project = session.query(Project).filter_by(id=id).first()
-                except Exception as e:
-                    error_decription = "获取project失败！\n"
-                    error_location = traceback.format_exc()
-                    mylogger.error(error_decription + error_location)
-                    raise e
+                project = session_in_api.query(Project).filter_by(id=id).first()
                 rst = make_response(jsonify({"success": True, "msg": "",
                                              "apiList": api_list,
                                              "projectInfo": {"name": project.name, "desc": project.desc},
@@ -101,8 +116,9 @@ class APILIst(Resource):
             else:
                 project_id = args["id"]
                 try:
-                    apis_obj = session.query(API).filter_by(project_id=project_id).all()
+                    apis_obj = session_in_api.query(API).filter_by(project_id=project_id).all()
                 except Exception as e:
+                    # 需要中间记录一下错误，可以按照下列做法
                     error_decription = "获取APIL所有数据失败！\n"
                     error_location = traceback.format_exc()
                     mylogger.error(error_decription + error_location)
@@ -114,15 +130,18 @@ class APILIst(Resource):
                 return rst
         except Exception as e:
             # 捕捉上面出错继续上抛的异常（做好log记录，便于异常时候排查）和其它可能的异常
-            session.rollback()  # 出错时，及时回滚数据库
+            # 出错时，及时回滚数据库
+            try:
+                session_in_api.rollback()  # 这里的Exception不一定时数据库错误，盲目执行会报错
+            except Exception as error:
+                pass
             mylogger.error("获取APIList失败！\n")
             rst = make_response(jsonify({"success": False, "msg": "获取APIList失败！" + str(e)}))
             return rst
 
-
     def delete(self):
         args = parser.parse_args()
-        status, msg = curd.delete_api(args["api_id"])
+        status, msg = curd.delete_api(args["api_id"])  # 这句会涉及数据库操作，但session是curd中的，对应出错时在那边进行回滚
         rst = make_response(jsonify({"success": status, "msg": msg}))
         return rst
 
@@ -154,10 +173,16 @@ class APILIst(Resource):
         args = parser.parse_args()
         apiForm = args["api_obj"]
         api_func, project_id = apiForm["def"], apiForm["project_id"]
-        obj = session.query(API).filter(API.api_func == api_func and API.project_id == project_id).first()
+        try:
+            obj = session_in_api.query(API).filter(API.api_func == api_func and API.project_id == project_id).first()
+        except Exception as e:
+            session_in_api.rollback()
+            rst = make_response(jsonify({"success": False, "msg": "API新增失败！"}))
+            return rst
         if obj is not None:
             rst = make_response(jsonify({"success": False, "msg": "该项目中已存在同名API调用！！！"}))
             return rst
+
         api = {
             "api":
                 {"request": {"method": apiForm["method"],
