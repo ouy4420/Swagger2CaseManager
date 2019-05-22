2bdf5eb9c6ce0dd21d8383ae4479882e07d51b4f
TigerJlx <2274701670@qq.com>
2019-05-22 11:23:55 +0800

    增加邮件发送编辑框进行测试报告邮件发送

--- frontend/src/components/main_view/report/report.vue
+++ frontend/src/components/main_view/report/report.vue
@@ -8,6 +8,56 @@
         </li>
       </ul>
     </el-header>
+    <el-dialog
+      title="邮件发送"
+      :visible.sync="DialogVisible"
+      width="30%"
+      align="center"
+      @close="reset_mail_form"
+    >
+      <el-form :model="mailForm"
+               :rules="rules"
+               ref="mailForm"
+               label-width="110px"
+               class="project">
+        <el-form-item label="发送方邮箱" prop="from">
+          <el-input placeholder="请输入发送方邮箱" v-model="mailForm.from"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱密码" prop="password">
+          <el-input placeholder="请输入密码" v-model="mailForm.password" show-password></el-input>
+        </el-form-item>
+        <el-form-item label="接收方邮箱" prop="to">
+          <el-autocomplete
+            style="width: 425px"
+            popper-class="my-autocomplete"
+            v-model="mailForm.to"
+            :fetch-suggestions="querySearch"
+            placeholder="请输入发送方"
+            @select="handleSelect">
+            <i
+              class="el-icon-edit el-input__icon"
+              slot="suffix"
+              @click="handleIconClick">
+            </i>
+            <template slot-scope="{ item }">
+              <div>{{ item.value }}</div>
+            </template>
+          </el-autocomplete>
+
+        </el-form-item>
+        <el-form-item label="抄送" prop="more">
+          <el-input placeholder="多个抄送方，请以分号隔开" v-model="mailForm.more"></el-input>
+        </el-form-item>
+        <el-form-item label="邮件描述" prop="description">
+          <el-input type="textarea" placeholder="请输入邮件描述" v-model="mailForm.description"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+                        <el-button @click="DialogVisible = false">取消</el-button>
+                        <el-button type="primary" @click="sendmail()">确 定</el-button>
+                      </span>
+    </el-dialog>
+
     <el-container>
       <el-main style="padding: 0; margin-top: 80px;margin-left: 10px; margin-right: 10px">
         <el-table
@@ -119,6 +169,12 @@
               </el-button>
               <el-button
                 size="medium"
+                type="warning"
+                icon="el-icon-message"
+                @click="get_report_info(scope.$index, scope.row)">邮件
+              </el-button>
+              <el-button
+                size="medium"
                 type="danger"
                 icon="el-icon-delete"
                 @click="handleDelete(scope.$index, scope.row)">删除
@@ -162,7 +218,6 @@
         </div>
       </el-footer>
     </el-container>
-
   </el-container>
 </template>
 
@@ -172,6 +227,15 @@
     components: {},
     data() {
       return {
+        DialogVisible: false,
+        mailForm: {
+          "report_id": "",
+          "from": "",
+          "password": "",
+          "to": "",
+          "more": "",
+          "description": ""
+        },
         projectInfo: {
           "name": "11",
           "desc": 22
@@ -181,10 +245,60 @@
           page_previous: null,
           page_next: 2
         },
-        reportList: []
+        reportList: [],
+        mail_list: [
+          {"value": "zhixiang.liu@waykichainhk.com"},
+          {"value": "ning.shen@waykichainhk.com"},
+          {"value": "shenshan.wang@waykichainhk.com"},
+          {"value": "qian.tan@waykichainhk.com"},
+          {"value": "linxin.jiang@waykichainhk.com"}
+        ],
+        rules: {
+          from: [
+            {required: true, message: '请输入邮件发送方', trigger: 'blur'},
+            {min: 1, max: 50, message: '最多不超过50个字符', trigger: 'blur'}
+          ],
+          password: [
+            {required: true, message: '请输入密码', trigger: 'blur'},
+            {min: 1, max: 50, message: '最多不超过50个字符', trigger: 'blur'}
+          ],
+          to: [
+            {required: true, message: '请输入邮件接收方', trigger: 'blur'},
+            {min: 1, max: 50, message: '最多不超过50个字符', trigger: 'blur'}
+          ],
+          description: [
+            {required: true, message: '请输入邮件描述', trigger: 'blur'},
+            {min: 1, max: 1000, message: '最多不超过1000个字符', trigger: 'blur'}
+          ]
+        }
       }
     },
     methods: {
+      get_report_info(index, row) {
+        this.DialogVisible = true;
+        this.mailForm.report_id = row.id
+      },
+      sendmail() {
+        this.DialogVisible = false;
+        this.$api.mailReport({"mail": this.mailForm, "id": this.mailForm.report_id}).then(resp => {
+          if (resp['success']) {
+            this.success(resp);
+          } else {
+            this.failure(resp);
+          }
+        });
+        this.reset_mail_form();
+      },
+      reset_mail_form() {
+        this.mailForm = {
+          "report_id": "",
+          "from": "",
+          "password": "",
+          "to": "",
+          "more": "",
+          "description": ""
+        }
+      },
       handleDelete(index, row) {
         // 弹出确认警告提示框
         this.$confirm('此操作将永久删除该报告, 是否继续?', '提示', {
@@ -269,6 +383,23 @@
             message: '取消输入'
           });
         });
+      },
+      querySearch(queryString, cb) {
+        var restaurants = this.mail_list;
+        var results = queryString ? restaurants.filter(this.createFilter(queryString)) : restaurants;
+        // 调用 callback 返回建议列表的数据
+        cb(results);
+      },
+      createFilter(queryString) {
+        return (restaurant) => {
+          return (restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+        };
+      },
+      handleSelect(item) {
+        console.log(item);
+      },
+      handleIconClick(ev) {
+        console.log(ev);
       }
     },
     mounted() {
@@ -278,5 +409,16 @@
 </script>
 
 <style scoped>
+
+  li {
+    line-height: normal;
+    padding: 7px;
+  }
+
+
+  .highlighted .addr {
+    color: #ddd;
+  }
+
 
 </style>
