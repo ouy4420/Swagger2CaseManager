a1fb94dc71adf41aecde35d81912f18faffae293
TigerJlx <2274701670@qq.com>
2019-06-06 16:26:33 +0800

    将httprunner1.5.15版本嵌入到SwaggerCaseManager中

---
+++ Myhttprunner/__init__.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+try:
+    # monkey patch at beginning to avoid RecursionError when running locust.
+    from gevent import monkey; monkey.patch_all()
+except ImportError:
+    pass
+
+from Myhttprunner.api import HttpRunner
